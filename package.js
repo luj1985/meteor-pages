2f2ad79826cd8984ce04746f90e5f762c2775b43
Lu, Jun <luj1985@gmail.com>
2015-01-01 20:06:32 +0800

    clean up

--- package.js
+++ package.js
@@ -6,68 +6,67 @@ Package.describe({
 });
 
 Package.onUse(function(api){
-    api.versionsFrom("METEOR@0.9.4")
-    api.use([
-        "check",
-        "tracker",
-        "underscore",
-        "coffeescript",
-        "mongo",
-        "ejson"
-    ]);
-    api.use("iron:router", ["client", "server"], { weak: true })
+  api.versionsFrom("METEOR@0.9.4")
+  api.use([
+    "check",
+    "tracker",
+    "underscore",
+    "coffeescript",
+    "mongo",
+    "ejson"
+  ]);
+  api.use("iron:router", ["client", "server"], { weak: true })
 
-    api.use([
-        "templating",
-        "spacebars",
-        "blaze",
-        "session"
-    ], "client");
+  api.use([
+    "templating",
+    "spacebars",
+    "blaze",
+    "session"
+  ], "client");
 
-    api.addFiles([
-        "lib/pages.coffee"
-    ]);
+  api.addFiles([
+    "lib/pages.coffee"
+  ]);
 
-    api.addFiles([
-        "client/templates.html",
-        "client/controllers.coffee",
-        "client/main.css",
-        "public/loader.gif"
-    ], "client");
+  api.addFiles([
+    "client/templates.html",
+    "client/controllers.coffee",
+    "client/main.css"
+  ], "client");
 });
 
 Package.onTest(function(api){
-    api.versionsFrom("METEOR@0.9.4")
-    api.use([
-        "tracker",
-        "underscore",
-        "coffeescript",
-        "mongo",
-        "ejson"
-    ]);
+  api.versionsFrom("METEOR@0.9.4")
+  api.use([
+    "tracker",
+    "underscore",
+    "coffeescript",
+    "mongo",
+    "ejson"
+  ]);
 
-    api.use([
-        "templating",
-        "spacebars",
-        "blaze",
-        "session"
-    ], "client");
+  api.use([
+    "templating",
+    "spacebars",
+    "blaze",
+    "session"
+  ], "client");
 
-    api.use([
-        "tracker",
-        "underscore",
-        "coffeescript",
-        "check"
-    ], "server");
+  api.use([
+    "tracker",
+    "underscore",
+    "coffeescript",
+    "check"
+  ], "server");
 
-    api.addFiles([
-        "lib/pages.coffee",
-        "test/tests.coffee"
-    ]);
+  api.addFiles([
+    "lib/pages.coffee",
+    "test/tests.coffee"
+  ]);
 
-    api.addFiles([
-        "client/templates.html",
-        "client/controllers.coffee",
-        "client/main.css"
-    ], "client");
+  api.addFiles([
+    "client/templates.html",
+    "client/controllers.coffee",
+    "client/main.css"
+  ], "client");
 });
